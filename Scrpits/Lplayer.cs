3a8694dcd4ef14e5de5fe6db9e618dc4ae50887e
Liu <738754564@qq.com>
2017-03-06 19:00:19 +0800

    juqing

---
+++ Scrpits/Lplayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Lplayer : MonoBehaviour
+{
+
+    Animator _ani;
+    public void Awake()
+    {
+        _ani = GetComponent<Animator>();
+        //die();
+    }
+
+    //死亡动画
+    public void die()
+    {
+        //Debug.Log("aq");
+        _ani.SetBool("Isdie", true);
+    }
+
+}
---
+++ Scrpits/Synopsis.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using LitJson;
+public class Synopsis : MonoBehaviour
+{
+    Button _btn;
+    // Animator ani;
+   
+    Text _text;
+    int i = 0;
+    JsonData jsd;
+    public void Awake()
+    {
+
+        //btn = GameObject.FindWithTag("btn").GetComponent<Button>();
+        _btn = transform.Find("Button").GetComponent<Button>();
+        _text = transform.Find("Text").GetComponent<Text>();
+        //Debug.Log(_btn);
+      //  ani = GameObject.FindWithTag("bb").GetComponent<Animator>();
+       
+        
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        TextAsset m_text = Resources.Load("JSONText") as TextAsset;
+        string str = m_text.text;
+
+        // Debug.LogError("str="+str);
+
+         jsd = JsonMapper.ToObject(str);
+        // Debug.Log(jsd.Count);
+        _btn.onClick.AddListener(A);
+        
+    
+    }
+
+    void A()
+    {
+       //文字提示
+        if (i < jsd.Count)
+        {
+            if (i==jsd.Count-1)
+            {
+                UiMgr._UiMgr._Player.die();
+            }
+          //  Debug.Log("mosterID=" + jsd[i]["mosterID"]);
+           // Debug.Log("Name=" + jsd[i]["MosterName"].ToString());
+            _text.text= jsd[i]["MosterName"].ToString();
+        }
+        i++;
+       // Debug.Log("aaaa");
+       // _player.die();
+
+    }
+    void Update()
+    {
+       
+    }
+}
